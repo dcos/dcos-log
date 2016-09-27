4a60bc33eb9911b7842d3c320add915d604005e4
Maksym Naboka <maksym.naboka@gmail.com>
2016-09-27 14:13:42 -0700

    add helpers
    - Dockerfile a docker image capable of building a project
    - top level Makefile
    - build.sh build script*
    - run.sh execute a container for dev*
    - minor README.md update
    
    *shell scripts are added for the time being. Eventually we will exclude them
    from the repo.

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Top level docker container used to build go projects.
+
+# Build all projects:
+# docker run -v $GOPATH:/go <contaier-id> make -C /go/src/github.com/dcos/dcos-go
+
+FROM golang:1.7.1
+
+RUN apt-get update && apt-get install -y \
+    # used by dcos-log
+    libsystemd-dev
+
+ENV PATH /go/bin:/usr/local/go/bin:$PATH
---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+IMG=$(docker images journal | grep journal | awk '{print $3}')
+
+if [ -z $IMG ] && [ -z $1 ]; then
+  echo "Could not detect docker image, please set tag 'journal' or pass image ID"
+  echo "$0 <image-id>"
+  exit 1
+fi
+
+[ -z $IMG ] && IMG=$1
+
+docker run -v $PWD:/go/src/github.com/dcos/dcos-go $IMG make -C /go/src/github.com/dcos/dcos-go
