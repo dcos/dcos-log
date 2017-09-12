12816a7cfbd53a593fd7c0891f5eaf80d551bcdd
Maksym Naboka <maksym.naboka@gmail.com>
2017-09-12 10:55:50 -0700

    add format.go

---
+++ dcos-log/mesos/files/reader/format.go
@@ -0,0 +1,14 @@
+package reader
+
+import "fmt"
+
+type Formatter func(l Line) string
+
+func sseFormat(l Line) (output string) {
+	if l.Offset > 0 && l.Size > 0 {
+		output += fmt.Sprintf("id: %d\n", l.Offset + l.Size)
+	}
+
+	output += fmt.Sprintf("data: %s\n\n", l.Message)
+	return output
+}
