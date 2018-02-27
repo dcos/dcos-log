f9ce2db75f3a7b155bbff7b0afcd39494e4263b7
Collin Van Dyck <collinvandyck@gmail.com>
2018-02-27 14:25:58 -0500

    Fix paths

--- dcos-log/scripts/test.sh
+++ dcos-log/scripts/test.sh
@@ -8,6 +8,9 @@
 set -e # exit on failure
 set -x # print each command
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
 cleanup() {
 	echo "Cleaning up the container..."
 	docker rm -f ${CONTAINER_NAME} >/dev/null
@@ -21,7 +24,7 @@ echo "Starting container that is running systemd and journald..."
 echo "current dir is ${CURRENT_DIR}"
 docker run \
 	-d \
-	-v ${CURRENT_DIR}:${PKG_DIR}/${BINARY_NAME} \
+	-v ${DIR}/../..:${PKG_DIR}/${BINARY_NAME} \
 	--privileged \
 	--rm \
 	--name ${CONTAINER_NAME}  \
@@ -32,6 +35,5 @@ echo "Running tests against that container..."
 docker exec \
 	${CONTAINER_NAME} \
 	bash -c 'cd /go/src/github.com/dcos/dcos-log &&
-		/usr/local/go/bin/go test -race -v $(go list ./...|grep -v vendor)'
-		#/usr/local/go/bin/go test -race -cover -test.v $(go list ./...|grep -v vendor)'
+		/usr/local/go/bin/go test -race -cover -test.v $(go list ./...|grep -v vendor)'
 
