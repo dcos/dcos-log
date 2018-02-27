bb7f852fcfa297401649e5cc3ff7219c77339bb8
Collin Van Dyck <collinvandyck@gmail.com>
2018-02-27 12:36:13 -0500

    Use a script to run the golang test suite.
    Running the tests requires three steps:
    
    1. Start a container running systemd/journald.
    2. Exec into the container to run the test suite.
    3. Cleanup the container.
    
    This was too difficult to do in the Makefile itself, so the
    Makefile will call this script when executing `make test`,
    passing in makefile variables as bash env variables.

---
+++ dcos-log/scripts/test.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# This script runs the tests for the dcos-log go code.  In order to provide
+# an environment where systemd/journald is running, it first starts up
+# a container running /sbin/init.  It then execs into that container to run
+# the go test suite. Before the script exits, it will destroy the container.
+
+set -e # exit on failure
+
+cleanup() {
+	echo "Cleaning up the container..."
+	docker rm -f ${CONTAINER_NAME} >/dev/null
+}
+trap cleanup EXIT
+
+echo "Starting container that is running systemd and journald..."
+docker run \
+	-d \
+	-v ${CURRENT_DIR}/..:${PKG_DIR}/${BINARY_NAME} \
+	--privileged \
+	--rm \
+	--name ${CONTAINER_NAME}  \
+	${IMAGE_NAME} \
+	/sbin/init >/dev/null
+
+echo "Running tests against that container..."
+docker exec \
+	-it \
+	-w /go/src/github.com/dcos/dcos-log/dcos-log \
+	${CONTAINER_NAME} \
+	go test -race -cover -test.v ./...
+
