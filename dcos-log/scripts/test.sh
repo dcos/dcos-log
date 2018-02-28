1de8f74cbcc0e27141a22d46b74307073b7cebaa
Collin Van Dyck <collinvandyck@gmail.com>
2018-02-28 16:00:46 -0500

    Add script to run the Go test suite for dcos-log.
    Because the process involves first setting up a container and
    starting systemd/journald, and then running tests by execing
    into the container, and finally cleaning up, it was easier to do
    all of this in a bash script instead of in the Makefile.

---
+++ dcos-log/scripts/test.sh
@@ -0,0 +1,37 @@
+# This script runs the tests for the dcos-log go code.  In order to provide
+# an environment where systemd/journald is running, it first starts up
+# a container running /sbin/init.  It then execs into that container to run
+# the go test suite. Before the script exits, it will destroy the container.
+
+set -e # exit on failure
+set -x # print each command
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/../..
+
+cleanup() {
+	echo "Cleaning up the container..."
+	docker rm -f ${CONTAINER_NAME} >/dev/null
+}
+trap cleanup EXIT
+
+echo "Starting container that is running systemd and journald..."
+echo "current dir is ${CURRENT_DIR}"
+docker run \
+	-d \
+	-v $(pwd):${PKG_DIR}/${BINARY_NAME} \
+	--privileged \
+	--rm \
+	--name ${CONTAINER_NAME}  \
+	${IMAGE_NAME} \
+	/sbin/init >/dev/null
+
+echo "Running tests against that container..."
+docker exec \
+	${CONTAINER_NAME} \
+	bash -c '\
+		cd /go/src/github.com/dcos/dcos-log && \
+		go install $(go list github.com/dcos/dcos-log/...|grep -v vendor) && \
+		go test -race -cover -test.v $(go list ./...|grep -v vendor)'
+
+
